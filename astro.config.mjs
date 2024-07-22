74f05726f657e19b2a73d150af9d920e098ef453
LMsqz <luisfmasaquiza@hotmail.com>
2024-07-22 14:00:46 +0200

    la primera commit

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+import icons from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [
+        icons()
+    ],
+});
---
+++ src/data/navData.ts
@@ -0,0 +1,34 @@
+export const siteData = {
+    name: "Galeria de caffe Rolia"
+}
+
+export const navData = [
+  {
+    name: "Shpi",
+    href:"shopi",
+  },
+  {
+    name: "Order nama",
+    href:"order-name",
+  },
+  {
+    name: "wholesale",
+    href: "wholesale",
+  },
+  {
+    name: "Home",
+    href:"/",
+  },
+  {
+    name: "About",
+    href:"about",
+  },
+  {
+    name: "Contacto",
+    href:"contact",
+  },
+  {
+    name: "Sign-in",
+    href:"Sing-in",
+  },
+]
